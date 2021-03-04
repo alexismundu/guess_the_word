c2feacf7091765b1ecd22fd1b24ba57b0de9083b
Alexis Munoz <alexismun53@gmail.com>
2021-03-03 22:49:46 -0600

    Add counter when user clicks skip and the guess word is different

--- lib/bloc/home_bloc.dart
+++ lib/bloc/home_bloc.dart
@@ -28,6 +28,8 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   List<String> list2;
   int _counter = 0;
   int _index = 0;
+  final String inGameTitle = "The word is...";
+  final String endGameTitle = "You Scored";
 
   @override
   Stream<HomeState> mapEventToState(
@@ -38,29 +40,33 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
       _counter = 0;
       list2 = new List<String>.from(list1);
       list2.shuffle();
-      yield JuegoIniciadoState(
-          contador: _counter, palabra: list1[_index], titulo: 'The word is...');
+      yield _instanceJuegoIniciado();
     } else if (event is SkipEvent) {
+      if (list1[_index] != list2[_index]) _counter++;
       _index++;
       if (_index < list1.length)
-        yield JuegoIniciadoState(
-            contador: _counter,
-            palabra: list1[_index],
-            titulo: "The word is....");
+        yield _instanceJuegoIniciado();
       else
         yield JuegoEndState(contador: _counter, titulo: "You Scored");
     } else if (event is GotEvent) {
       if (list1[_index] == list2[_index]) _counter++;
       _index++;
-      if (_index < list1.length)
-        yield JuegoIniciadoState(
-            contador: _counter,
-            palabra: list1[_index],
-            titulo: "The word is....");
-      else
-        yield JuegoEndState(contador: _counter, titulo: "You Scored");
+      if (_index < list1.length) {
+        yield _instanceJuegoIniciado();
+      } else {
+        yield _instanceJuegoEndState();
+      }
     } else if (event is EndEvent) {
-      yield JuegoEndState(contador: _counter, titulo: "You Scored");
+      yield _instanceJuegoEndState();
     }
+  }
+
+  JuegoIniciadoState _instanceJuegoIniciado() {
+    return JuegoIniciadoState(
+        contador: _counter, palabra: list1[_index], titulo: inGameTitle);
+  }
+
+  JuegoEndState _instanceJuegoEndState() {
+    JuegoEndState(contador: _counter, titulo: endGameTitle);
   }
 }
